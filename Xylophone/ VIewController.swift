ebcd9aaa590ecb200e5ff94550c64a8a905bae0e
Philip Yu <pyu3@mail.sfsu.edu>
2019-06-28 17:46:47 -0700

    Added sound to tap motion on xylophone

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    // Properties
+    var audioPlayer: AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
     }
-
-
-
+    
     @IBAction func notePressed(_ sender: UIButton) {
         
+        var selectedSoundFileName: String = soundArray[sender.tag - 1]
         
+        playSound(fileName: selectedSoundFileName)
         
     }
     
-  
-
+    func playSound(fileName: String) {
+        
+        let soundURL = Bundle.main.url(forResource: fileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+        
+    }
+    
 }
 
